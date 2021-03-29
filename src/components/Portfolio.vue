7833eb1c0faadc6f755ad18f969377f80a3272b1
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2021-03-29 18:33:27 +0100

    Portfolio images

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,8 +1,7 @@
 <template>
   <h1>Portfolio</h1>
-  <h2>Look what I can do</h2>
   <Carousel ref="carousel" :autoplay="autoplay" class="mt-5">
-    <div v-for="image in images" :key="image" class="agile__slide" @click="showLightbox">
+    <div v-for="image in thumbs" :key="image" class="agile__slide" @click="showLightbox">
       <img :alt="image" :src="image">
     </div>
   </Carousel>
@@ -16,14 +15,26 @@ import VueEasyLightBox from "vue-easy-lightbox";
 
 import {Carousel} from "./carousel"
 
-import slide1 from "../assets/slide1.png"
-import slide2 from "../assets/slide2.jpg"
+import slide1 from "../assets/slide_1.jpg"
+import slide2 from "../assets/slide_2.jpg"
+import slide3 from "../assets/slide_3.jpg"
+import slide4 from "../assets/slide_4.jpg"
+import slide5 from "../assets/slide_5.jpg"
+import slide6 from "../assets/slide_6.jpg"
+
+import thumb1 from "../assets/slide_1_thumb.jpg"
+import thumb2 from "../assets/slide_2_thumb.jpg"
+import thumb3 from "../assets/slide_3_thumb.jpg"
+import thumb4 from "../assets/slide_4_thumb.jpg"
+import thumb5 from "../assets/slide_5_thumb.jpg"
+import thumb6 from "../assets/slide_6_thumb.jpg"
 
 export default {
   name: "Portfolio",
   components: {Carousel, VueEasyLightBox},
   setup() {
-    const images = ref([slide1, slide2])
+    const images = ref([slide1, slide2, slide3, slide4, slide5, slide6])
+    const thumbs = ref([thumb1, thumb2, thumb3, thumb4, thumb5, thumb6])
     const index = ref(0)
     const lightboxVisible = ref(false)
     const autoplay = ref(true)
@@ -45,7 +56,7 @@ export default {
     }
 
     return {
-      images, index, lightboxVisible, autoplay, carousel, showLightbox, hideLightbox, slideChanged
+      images, thumbs, index, lightboxVisible, autoplay, carousel, showLightbox, hideLightbox, slideChanged
     }
   }
 }
