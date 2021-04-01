74be30a570993dae4bcf36dc65a3fae72ffe432d
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2021-04-01 08:52:40 +0100

    Airbrake

--- src/main.js
+++ src/main.js
@@ -3,14 +3,29 @@ import VueSmoothScroll from 'vue3-smooth-scroll'
 import VueEasyLightBox from "vue-easy-lightbox";
 import Carousel from './components/carousel'
 import {VueReCaptcha} from 'vue-recaptcha-v3'
+import {Notifier} from '@airbrake/browser';
 
 import App from './App.vue'
 import './index.css'
 
 // noinspection JSCheckFunctionSignatures
-createApp(App)
+const app = createApp(App)
   .use(VueSmoothScroll, {offset: -20})
   .use(Carousel)
   .use(VueEasyLightBox)
   .use(VueReCaptcha, {siteKey: "6LdzI5caAAAAABGKERpWx2URDf9Tye0KE2odzJVB"})
-  .mount('#app')
+
+const airbrake = new Notifier({
+  environment: 'production',
+  projectId: 328313,
+  projectKey: '8baa31e65729a99d9aa04b72ef8dd9ee'
+});
+
+app.config.errorHandler = (error, _vm, info) => {
+  airbrake.notify({
+    error,
+    params: {info}
+  });
+}
+
+app.mount('#app')
