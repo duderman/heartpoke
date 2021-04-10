bd4a5598b9a3930e9c4954edea1ef45908d0189d
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2021-04-10 20:25:32 +0100

    Header, hero, page bg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
 <section id="header" class="h-screen w-screen bg-cover bg-center flex flex-col items-center">
+  <video poster="../assets/header-bg.jpeg" class="absolute h-full object-cover w-full" autoplay muted loop>
+    <source src="../assets/header-bg.mp4" type="video/mp4">
+  </video>
   <div class="self-stretch mt-auto">
-    <h1 class="text-white uppercase" data-aos="fade-down">HeartPoke</h1>
-    <h2 class="font-medium text-gray-200 mt-2 p-2 lowercase" data-aos="fade-down">Handpoke tattoo artist</h2>
+    <h1 class="text-white uppercase stroke-1 with-stroke" data-aos="fade-down">Heart.Poke</h1>
+    <h2 class="font-medium text-gray-200 mt-2 p-2 lowercase with-stroke lighter" data-aos="fade-down">Handpoke tattoo artist</h2>
     <Button class="mt-5 uppercase" data-aos="fade-up" url="#book">
       Book now
     </Button>
@@ -22,7 +25,14 @@ export default {
 </script>
 
 <style scoped>
-#header {
-  background-image: url("../assets/header-bg2.jpeg");
+.with-stroke {
+  --stroke-opacity: 0.3;
+  --stroke-width: 2px;
+  -webkit-text-stroke: var(--stroke-width) rgba(190,24,93, var(--stroke-opacity));
+}
+
+.with-stroke.lighter {
+  --stroke-opacity: 0.1;
+  --stroke-width: 1px;
 }
 </style>
