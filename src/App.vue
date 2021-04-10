23b3fd7f6f8d6768309dd18689575d7ac96770aa
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2021-04-10 18:57:06 +0100

    Remove why section

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <Header/>
   <Hero/>
   <Portfolio/>
-  <Why/>
   <Book/>
   <Studio/>
 </template>
@@ -14,13 +13,12 @@ import 'aos/dist/aos.css'
 import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
 import Portfolio from './components/Portfolio.vue'
-import Why from './components/Why.vue'
 import Book from './components/Book.vue'
 import Studio from './components/Studio.vue'
 
 export default {
   name: "App",
-  components: {Header, Hero, Portfolio, Why, Book, Studio},
+  components: {Header, Hero, Portfolio, Book, Studio},
   created() {
     AOS.init({
       duration: 1000
