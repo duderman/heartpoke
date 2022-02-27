f125f14250b66b78b9819f340066eb964bf2afd2
nikolay.norkin <nikolay.norkin@fresha.com>
2022-02-27 17:26:39 +0000

    new portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -39,6 +39,19 @@ import slide4 from "../assets/slide_4.jpg";
 import slide5 from "../assets/slide_5.jpg";
 import slide6 from "../assets/slide_6.jpg";
 import slide7 from "../assets/slide_7.jpg";
+import slide8 from "../assets/slide_8.jpg";
+import slide9 from "../assets/slide_9.jpg";
+import slide10 from "../assets/slide_10.jpg";
+import slide11 from "../assets/slide_11.jpg";
+import slide12 from "../assets/slide_12.jpg";
+import slide13 from "../assets/slide_13.jpg";
+import slide14 from "../assets/slide_14.jpg";
+import slide15 from "../assets/slide_15.jpg";
+import slide16 from "../assets/slide_16.jpg";
+import slide17 from "../assets/slide_17.jpg";
+import slide18 from "../assets/slide_18.jpg";
+import slide19 from "../assets/slide_19.jpg";
+import slide20 from "../assets/slide_20.jpg";
 
 import thumb1 from "../assets/slide_1_thumb.jpg";
 import thumb2 from "../assets/slide_2_thumb.jpg";
@@ -47,6 +60,19 @@ import thumb4 from "../assets/slide_4_thumb.jpg";
 import thumb5 from "../assets/slide_5_thumb.jpg";
 import thumb6 from "../assets/slide_6_thumb.jpg";
 import thumb7 from "../assets/slide_7_thumb.jpg";
+import thumb8 from "../assets/slide_8_thumb.jpg";
+import thumb9 from "../assets/slide_9_thumb.jpg";
+import thumb10 from "../assets/slide_10_thumb.jpg";
+import thumb11 from "../assets/slide_11_thumb.jpg";
+import thumb12 from "../assets/slide_12_thumb.jpg";
+import thumb13 from "../assets/slide_13_thumb.jpg";
+import thumb14 from "../assets/slide_14_thumb.jpg";
+import thumb15 from "../assets/slide_15_thumb.jpg";
+import thumb16 from "../assets/slide_16_thumb.jpg";
+import thumb17 from "../assets/slide_17_thumb.jpg";
+import thumb18 from "../assets/slide_18_thumb.jpg";
+import thumb19 from "../assets/slide_19_thumb.jpg";
+import thumb20 from "../assets/slide_20_thumb.jpg";
 
 export default {
   name: "Portfolio",
@@ -60,6 +86,19 @@ export default {
       slide5,
       slide6,
       slide7,
+      slide8,
+      slide9,
+      slide10,
+      slide11,
+      slide12,
+      slide13,
+      slide14,
+      slide15,
+      slide16,
+      slide17,
+      slide18,
+      slide19,
+      slide20,
     ]);
     const thumbs = ref([
       thumb1,
@@ -69,6 +108,19 @@ export default {
       thumb5,
       thumb6,
       thumb7,
+      thumb8,
+      thumb9,
+      thumb10,
+      thumb11,
+      thumb12,
+      thumb13,
+      thumb14,
+      thumb15,
+      thumb16,
+      thumb17,
+      thumb18,
+      thumb19,
+      thumb20,
     ]);
     const index = ref(0);
     const lightboxVisible = ref(false);
