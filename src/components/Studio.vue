c95c73eab899b626d16f42cfae01a0ab8e91b940
nikolay.norkin <nikolay.norkin@fresha.com>
2022-07-09 23:05:57 +0100

    Change studio

--- src/components/Studio.vue
+++ src/components/Studio.vue
@@ -1,15 +1,15 @@
 <template>
   <h1 class="mb-2">Studio</h1>
 
-  <div class="text-2xl">VaporWave Tattoo</div>
-  <div>5 Crown Works, Temple Street, London E2 6QQ</div>
+  <div class="text-2xl">Sixty Ink</div>
+  <div>First Floor, 28 Denmark St, London WC2H 8NJ</div>
 
   <iframe id="studio-map" ref="iframe" class="w-screen mt-5 h-96" />
 </template>
 
 <script>
 const STUDIO_MAP_SRC =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.0490280892845!2d-0.06281794870226565!3d51.53066057953909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761d80e8f1a3f3%3A0x424810f9b823e47b!2sVaporwave%20Studios!5e0!3m2!1sen!2suk!4v1613521689253!5m2!1sen!2suk";
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.8709898152883!2d-0.12900879999999998!3d51.5155828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b1e8c4216ed%3A0xa932b5be899c751d!2sSixty%20Ink!5e0!3m2!1sen!2sru!4v1657404087554!5m2!1sen!2sru";
 export default {
   name: "Studio",
   mounted() {
