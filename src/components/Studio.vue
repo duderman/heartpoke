50f8ca18d7d8b112a1dee9971a36e8b7e9b72d28
nikolay.norkin <nikolay.norkin@fresha.com>
2022-09-12 18:14:49 +0200

    Change address

--- src/components/Studio.vue
+++ src/components/Studio.vue
@@ -1,15 +1,15 @@
 <template>
   <h1 class="mb-2">Studio</h1>
 
-  <div class="text-2xl">Sixty Ink</div>
-  <div>First Floor, 28 Denmark St, London WC2H 8NJ</div>
+  <div class="text-2xl">Blank Collective</div>
+  <div>Unit 1, 82-84 Childers Street London, United Kingdom SE8 5FS</div>
 
   <iframe id="studio-map" ref="iframe" class="w-screen mt-5 h-96" />
 </template>
 
 <script>
 const STUDIO_MAP_SRC =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.8709898152883!2d-0.12900879999999998!3d51.5155828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b1e8c4216ed%3A0xa932b5be899c751d!2sSixty%20Ink!5e0!3m2!1sen!2sru!4v1657404087554!5m2!1sen!2sru";
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.689333625457!2d-0.037129584273627315!3d51.482216520365306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876031dd4b0b3e7%3A0xac53eca7a1c407e3!2sBlank%20Collective!5e0!3m2!1sen!2s!4v1662997414757!5m2!1sen!2s";
 export default {
   name: "Studio",
   mounted() {
